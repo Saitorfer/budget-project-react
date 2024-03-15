10bc68c2e5c3cd7cd7218dfe0edfb8da0dba762a
Saitorfer <39096665+Saitorfer@users.noreply.github.com>
2024-03-15 23:38:40 +0100

    Added grafic component
    added with the librairy : npm i react-circular-progressbar

--- src/components/ControlBudget.jsx
+++ src/components/ControlBudget.jsx
@@ -1,43 +1,63 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar,buildStyles } from "react-circular-progressbar"
+import "react-circular-progressbar/dist/styles.css"
 
+const ControlBudget = ({ spends, budget }) => {
 
-const ControlBudget = ({spends, budget}) => {
+  const [available, setAvailable] = useState(0);
+  const [spended, setSpended] = useState(0);
+  const [percentage, setPercentage] = useState(0);
 
-    const [available,setAvailable] = useState(0);
-    const [spended,setSpended] = useState(0);
+  useEffect(() => {
+    const totalSpended = spends.reduce((total, spend) => spend.amount + total, 0);
+    const totalAvailable = budget - totalSpended;
 
-    useEffect(()=>{
-      const totalSpended = spends.reduce( (total, spend ) => spend.amount + total, 0);
-      const totalAvailable = budget - totalSpended;
+    // calculate percentage 
+    //the toFixed to get the .00
+    const newPercentage = (((budget - totalAvailable) / budget) * 100).toFixed(2)
 
-      setSpended(totalSpended);
-      setAvailable(totalAvailable);
+    setSpended(totalSpended);
+    setAvailable(totalAvailable);
 
-    },[spends])
+    //to animate a little later, we get a cool effect
+    setTimeout(() => {
+      setPercentage(newPercentage);
+    }, 800);
 
-    //format to euros
-    const formatBudgetToEuros = (amount) =>{
-        return amount.toLocaleString('en-US',{
-            style: 'currency',
-            currency: 'EUR'
-        })
-    }
+  }, [spends])
+
+  //format to euros
+  const formatBudgetToEuros = (amount) => {
+    return amount.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'EUR'
+    })
+  }
 
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>Grafica</p>
+        <CircularProgressbar
+          //change color
+          styles={buildStyles({
+            pathColor:"#3B82F6",
+            trailColor: "#F5F5F5",
+            textColor:"#3B82F6"
+          })}
+          value={percentage}
+          text={`${percentage}% Spended`}
+        />
       </div>
       <div className="contenido-presupuesto">
-            <p>
-                <span>Budget: </span> {formatBudgetToEuros(budget)}
-            </p>
-            <p>
-                <span>Avaible: </span> {formatBudgetToEuros(available)}
-            </p>
-            <p>
-                <span>Spended: </span> {formatBudgetToEuros(spended)}
-            </p>
+        <p>
+          <span>Budget: </span> {formatBudgetToEuros(budget)}
+        </p>
+        <p>
+          <span>Avaible: </span> {formatBudgetToEuros(available)}
+        </p>
+        <p>
+          <span>Spended: </span> {formatBudgetToEuros(spended)}
+        </p>
       </div>
     </div>
   )
